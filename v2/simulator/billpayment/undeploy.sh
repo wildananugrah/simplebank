bac256d01ed70481519d3822ab0cda50c813f052
wildananugrah <wildananugra@student.unimelb.edu.au>
2022-03-01 19:03:41 +1100

    update for mrc env

--- v2/simulator/billpayment/undeploy.sh
+++ v2/simulator/billpayment/undeploy.sh
@@ -1,4 +1,4 @@
-docker network disconnect simplebank-net sim_billpayment
+# docker network disconnect simplebank-net sim_billpayment
 docker container stop sim_billpayment
 docker container rm sim_billpayment
 docker image rm sim_billpayment:1.0
