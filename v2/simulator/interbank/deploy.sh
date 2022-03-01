b1cf3d08360f0058aa14431c9dd6a8a3a2bc634a
wildananugrah <wildananugra@student.unimelb.edu.au>
2022-03-01 18:51:30 +1100

    update for mrc env

--- v2/simulator/interbank/deploy.sh
+++ v2/simulator/interbank/deploy.sh
@@ -1,3 +1,4 @@
 docker build -t sim_interbank:1.0 . 
 # docker run -d --name sim_interbank --net simplebank-net -p 9000:9000 sim_interbank:1.0
-docker run -d --name sim_interbank -p 9000:9000 sim_interbank:1.0
+# docker run -d --name sim_interbank -p 9000:9000 sim_interbank:1.0
+docker run -d --name sim_interbank -p 3000:9000 sim_interbank:1.0
