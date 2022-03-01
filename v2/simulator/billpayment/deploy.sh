94f003f82eb98e12210e7652c87ed71c63f71f02
wildananugrah <wildananugra@student.unimelb.edu.au>
2022-03-01 19:05:48 +1100

    update for mrc env

--- v2/simulator/billpayment/deploy.sh
+++ v2/simulator/billpayment/deploy.sh
@@ -1,3 +1,3 @@
 docker build -t sim_billpayment:1.0 . 
 # docker run -d --name sim_billpayment --net simplebank-net -p 9010:9010 sim_billpayment:1.0
-docker run -d --name sim_billpayment -p 9010:9010 sim_billpayment:1.0
+docker run -d --name sim_billpayment -p 3010:9010 sim_billpayment:1.0
