ac598032dddb3578665c9d5d7a700e9a252a4e63
wildananugrah <wildananugra@student.unimelb.edu.au>
2022-03-01 19:29:43 +1100

    update for mrc env

--- v2/monolith-app/nginx/deploy.sh
+++ v2/monolith-app/nginx/deploy.sh
@@ -1,3 +1,2 @@
 docker build -t monolith-nginx:1.0 .
-docker run -d --name monolith-nginx -p 4000:80 monolith-nginx:1.0
-docker network connect simplebank-net monolith-nginx
+docker run -d --name monolith-nginx -net simplebank-net -p 4000:80 monolith-nginx:1.0
